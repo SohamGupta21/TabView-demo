b7f63669a3c5283bda460106ab2d61d84858bda6
Soham Gupta <guptasoham@outlook.com>
2021-12-06 06:56:22 -0600

    Initial Commit

---
+++ UI product/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UI product
+//
+//  Created by soham gupta on 12/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UI product/UI_productApp.swift
@@ -0,0 +1,17 @@
+//
+//  UI_productApp.swift
+//  UI product
+//
+//  Created by soham gupta on 12/6/21.
+//
+
+import SwiftUI
+
+@main
+struct UI_productApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
